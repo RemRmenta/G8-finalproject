d1951f0232960124af8cb8035f6356037989ba71
RemRmenta <remaarmenta@gmail.com>
2025-05-11 20:35:29 +0800

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 import { Card, CardContent } from '@/components/ui/card'
 
 type JsonUser = {
@@ -84,6 +85,14 @@ const LoginPage = () => {
                 Login
               </button>
             </form>
+
+            {/* Register Link */}
+            <p className="text-center mt-6 text-white text-base">
+              Don&apos;t have an account?{' '}
+              <Link href="/register" className="text-[#FFD6D5] hover:underline font-semibold">
+                Register here
+              </Link>
+            </p>
           </CardContent>
         </Card>
       </div>
